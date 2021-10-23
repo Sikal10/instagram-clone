2adc0612a5400a970465c64b27e2b94885270573
Sikal <sikal.sikal.ss@gmail.com>
2021-10-23 17:25:42 +0100

    React recoil setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import {SessionProvider} from "next-auth/react";
 import '../styles/globals.css'
+import {RecoilRoot} from "recoil";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+  return <SessionProvider session={session}>
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  </SessionProvider>
 }
 
 export default MyApp
